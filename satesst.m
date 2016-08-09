1650b739b37500ebfb7883e24e5a76dc8cfe3d51
jcvdav <juanarlos.villader@gmail.com>
2016-08-09 13:42:24 -0500

    Actualicé la función de descargar imágenes de Diego

--- satesst.m
+++ satesst.m
@@ -1,7 +1,11 @@
 function f = satesst(year)
 
 
-F1 = ftp('podaac-ftp.jpl.nasa.gov');
-data_save = '~';
-filen= 
-['/OceanTemperature/modis/L3/Aqua/4um/v2014.0/4km/monthly',num2str(year),'/'A,num2str(year),''0012011031.L3m_MO_SST4_sst4_4km.nc']
+F1 = ftp('podaac-ftp.jpl.nasa.gov'); %crear el ftp
+data_save = '~'; %Directorio donde se guarda
+file = ['/OceanTemperature/modis/L3/Aqua/4um/v2014.0/4km/monthly',num2str(year),'/A',num2str(year),'0012011031.L3m_MO_SST4_sst4_4km.nc'] %archivo que buscamos
+
+mget(f1,filee,data_save); % Se descarga el archivo con mget
+close(f1) %cierra el ftm
+
+%  ftp://podaac-ftp.jpl.nasa.gov/OceanTemperature/modis/L3/aqua/4um/v2014.0/4km/monthly/2003/
