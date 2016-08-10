bf4a9504d1d7c2743c0f6b02d9ef8b6a314b0b69
jcvdav <juanarlos.villader@gmail.com>
2016-08-09 21:15:50 -0700

    Agregué una imagen del 2003 y modifiqué un poco la función satesst()

--- Scripts/satesst.m
+++ Scripts/satesst.m
@@ -4,11 +4,12 @@ data_save = pwd(); %Directorio donde se guarda
 
 for i =1:12
     month=i;
-    %en la concatenación de abajo incluir mes
-    file = ['/OceanTemperature/modis/L3/aqua/4um/v2014.0/4km/monthly/',num2str(year),'/','A',num2str(year),'0012012031.L3m_MO_SST4_sst4_4km.nc'];
+    %En la concatenación de abajo incluir mes
+    file = ['/OceanTemperature/modis/L3/aqua/4um/v2014.0/4km/monthly/',num2str(year),'/','A',num2str(year),'001',num2str(year),'031.L3m_MO_SST4_sst4_4km.nc'];
+    mget(F1,file,data_save); % Se descarga el archivo con mget
+    close(F1) %cierra el ftp
 end
-mget(F1,file,data_save); % Se descarga el archivo con mget
-close(f1) %cierra el ftp
+end
 
 % ftp://podaac-ftp.jpl.nasa.gov/OceanTemperature/modis/L3/aqua/4um/v2014.0/4km/monthly/2003/
 
