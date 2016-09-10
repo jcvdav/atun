c945406ef237188c79609444530bcf28bb41eb6e
jcvdav <juanarlos.villader@gmail.com>
2016-09-10 15:13:18 -0700

    Imágenes convertidas a *.csv

--- Scripts/ndc2csv.m
+++ Scripts/ndc2csv.m
@@ -5,7 +5,7 @@ clc
 xlim = [720, 2520];
 ylim = [1200, 2160];
 
-for i = 2003:2014
+for i = 2005:2014
     for j = 1:12
         img_in = ['Datos/Oc/Imagenes/',num2str(i), '_', num2str(j),'.nc'];
         img = subset(img_in, xlim, ylim);
@@ -17,7 +17,8 @@ for i = 2003:2014
         img2(:,2) = j;
         img2(:,3:end) = img;
         
-        img_out = ['../Datos/Oc/imagenes/',num2str(i), '_', num2str(j),'.csv'];
-        csvwrite('Test2.csv', img2)
+        img_out = ['Datos/Oc/Imagenes/',num2str(i), '_', num2str(j),'.csv'];
+        csvwrite(img_out, img2)
+        disp(img_out)
     end
 end
